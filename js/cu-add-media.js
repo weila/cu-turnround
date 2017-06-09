3a94347d7441710e1809659a63499c6a27276ec0
weila <weila_wei@hotmail.com>
2017-06-09 23:21:12 +0800

    support matrix sprite image

--- js/cu-add-media.js
+++ js/cu-add-media.js
@@ -15,7 +15,7 @@ jQuery(function($) {
             var self = this; // Needed to retrieve our variable in the anonymous function below
             this.window.on('select', function() {
                     var mediaInfo = self.window.state().get('selection').first().toJSON();
-                    wp.media.editor.insert('<div class="cu-turnround-default" style="background:url('+mediaInfo.url+');width:20%;padding-bottom:20%;"></div>');
+                    wp.media.editor.insert('<div class="cu-turnround-default" style="background:url('+mediaInfo.url+');width:400px;padding-bottom:300px;"></div>');
                 });
         }
      
